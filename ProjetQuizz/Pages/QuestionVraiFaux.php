9df84e0cc52b18c36279fc8b17b83de5e605036d
shadokmental <56264083+shadokmental@users.noreply.github.com>
2020-03-10 10:06:32 +0100

    oii

---
+++ ProjetQuizz/Pages/QuestionVraiFaux.php
@@ -0,0 +1,5 @@
+<?php
+require_once "../Includes/functions.php";
+require_once "../Includes/head.php"; 
+require_once "../Includes/header.php";
+session_start();
--- projetQuizz/Includes/header.php
+++ projetQuizz/Includes/header.php
@@ -2,7 +2,7 @@
 
 <header>
 	<nav class="navbar navbar-light bg-light navbar-expand-lg">
-		<a class="navbar-brand" href="#">Page d'accueil</a>
+		<a class="navbar-brand" href="PageChoix.php">Page d'accueil</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
