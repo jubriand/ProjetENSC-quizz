7b1bfd0c7c3f98804d69e2111f9c00e5219d2d73
wsaunders12 <61310523+wsaunders12@users.noreply.github.com>
2020-04-01 20:12:26 +0200

    0

--- projetQuizz/Includes/footer.php
+++ projetQuizz/Includes/footer.php
@@ -1,3 +1,3 @@
-<footer class="footer">
-    <p class="text-center"> Construit avec ❤ par Willy la menace et Juju les gros bras pour un projet de l'<a href="https://www.ensc.fr">ENSC</a>.</p>
+<footer class="footer text-center">
+    Construit avec ❤ par Willy la menace et Juju les gros bras pour un projet de l'<a href="https://www.ensc.fr">ENSC</a>.
 </footer>
