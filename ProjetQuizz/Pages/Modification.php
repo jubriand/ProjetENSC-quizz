57654538f37d08bba743e6de8de37bf07fa6357d
wsaunders12 <61310523+wsaunders12@users.noreply.github.com>
2020-04-07 16:45:50 +0200

    0

--- ProjetQuizz/Pages/Modification.php
+++ ProjetQuizz/Pages/Modification.php
@@ -42,61 +42,63 @@ $infoBDD=$stmt->fetch();
 <html>
 	<body>
 	<?php require_once "../Includes/header.php" ;?>
-		<div class="container-fluid"> <br/>
-            <?php
-            if(!empty($_POST['pastInfo']) and !empty($_POST['newInfo']) and! empty($_POST['confirmInfo']))
-            {
-                if($_POST['pastInfo']==$infoBDD[$modif])
+        <div class="container-fluid"> <br/>
+            <div class="jumbotron">
+                <?php
+                if(!empty($_POST['pastInfo']) and !empty($_POST['newInfo']) and! empty($_POST['confirmInfo']))
                 {
-                    if($_POST['newInfo']==$_POST['confirmInfo'])
+                    if($_POST['pastInfo']==$infoBDD[$modif])
                     {
-                        $newInfo=escape($_POST['newInfo']);
-                        $stmt = getDb()->prepare("update `" . $table . "` set `" . $modif . "`=? where `" . $primKey. "`=?");
-                        $stmt->execute(array($newInfo, $id));
-                        if($modif=='PSEUDO')
+                        if($_POST['newInfo']==$_POST['confirmInfo'])
                         {
-                            $_SESSION['login'] = $_POST['newInfo'];
+                            $newInfo=escape($_POST['newInfo']);
+                            $stmt = getDb()->prepare("update `" . $table . "` set `" . $modif . "`=? where `" . $primKey. "`=?");
+                            $stmt->execute(array($newInfo, $id));
+                            if($modif=='PSEUDO')
+                            {
+                                $_SESSION['login'] = $_POST['newInfo'];
+                            }
+                            redirect($origine);
                         }
-                        redirect($origine);
+                        else
+                        {?>
+                            <div class="alert alert-danger" role="alert">
+                                <img src="../Icons/svg/warning.svg" alt="warning">
+                                La confirmation ne correspond pas au nouvel élément rentré
+                            </div>
+                        <?php }
                     }
                     else
                     {?>
                         <div class="alert alert-danger" role="alert">
                             <img src="../Icons/svg/warning.svg" alt="warning">
-                            La confirmation ne correspond pas au nouvel élément rentré
+                            L'ancien élément renseigné n'est pas valide
                         </div>
                     <?php }
-                }
-                else
-                {?>
-                    <div class="alert alert-danger" role="alert">
-                        <img src="../Icons/svg/warning.svg" alt="warning">
-                        L'ancien élément renseigné n'est pas valide
-                    </div>
-                <?php }
+                    
+                } ?>
+                <form method ="POST">
                 
-            } ?>
-            <form method ="POST">
-            
-            <fieldset ><legend class="text-center"> <h3> <span class="title">Modification d'un élément </span></h3> </legend>
-            <br/>
-            <div class=text-center>
-                <label for ="pastInfo"> Rentrez l'ancien élément: </label>
-                <?php if($table!="UTILISATEUR")
-                { ?>
-                    <input type="text" name="pastInfo" value="<?=$infoBDD[$modif]?>" size ="30"/> <br/><br/>
-                <?php }
-                else
-                {?>
-                    <input type="text" name="pastInfo" size ="30"/> <br/><br/>
-                <?php } ?>
-                <label for ="newInfo"> Rentrez le nouvel élément: </label>
-                <input type="text" name="newInfo" size="30"/> <br/>
-                <label for ="confirmInfo"> Veuillez confirmer le nouvel élément: </label>
-                <input type="text" name="confirmInfo" size="30"/><br/>
-                <br/><button type="submit" class="btn btn-primary">Modifier</button>
+                <fieldset ><legend class="text-center"> <h3> <span class="title">Modification d'un élément </span></h3> </legend>
+                <br/>
+                <div class=text-center>
+                    <label for ="pastInfo"> Rentrez l'ancien élément: </label>
+                    <?php if($table!="UTILISATEUR")
+                    { ?>
+                        <input type="text" name="pastInfo" value="<?=$infoBDD[$modif]?>" size ="30"/> <br/><br/>
+                    <?php }
+                    else
+                    {?>
+                        <input type="text" name="pastInfo" size ="30"/> <br/><br/>
+                    <?php } ?>
+                    <label for ="newInfo"> Rentrez le nouvel élément: </label>
+                    <input type="text" name="newInfo" size="30"/> <br/>
+                    <label for ="confirmInfo"> Veuillez confirmer le nouvel élément: </label>
+                    <input type="text" name="confirmInfo" size="30"/><br/>
+                    <br/><button type="submit" class="btn btn-primary">Modifier</button>
+                </div>
+                </form>
             </div>
-            </form>
 		</div>
 		<?php require_once "../Includes/footer.php"; ?> 
 		<?php require_once "../Includes/scripts.php"; ?> 
